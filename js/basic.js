94d210c21c120fd951ad3e386206da89664e7e5a
Lein6927 <lightnine861108@gmail.com>
2022-07-27 14:24:23 +0800

    first commit

---
+++ js/basic.js
@@ -0,0 +1,14 @@
+function numplus() {
+  let num = 0;
+  for (var i = 0; i <= 200; i++) {
+    num = i + 1;
+  }
+  console.log(num);
+  document.getElementById("num");
+}
+
+export default function numplus() {
+  {
+    num;
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,7 @@
+function numloop(num) {
+  let a1 = "555";
+  for (i = 0; i <= 100; i++) {
+    num = i;
+  }
+}
+export default function numloop(num) {}
